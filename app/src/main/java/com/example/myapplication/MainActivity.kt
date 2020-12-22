f8fcb2657955bf79c1a9bfe41b6fc1831f175b85
aira0046 <sygdn96@au.com>
2020-12-22 21:57:58 +0900

    画面遷移メインからグループ

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        //nyannyan
+        originalbutton.setOnClickListener {
+            val toRealmIdeaApplicationIntent = Intent(this, RealmIdeaApplication::class.java)
+            startActivities(toRealmIdeaApplicationIntent)
+        }
     }
 }
