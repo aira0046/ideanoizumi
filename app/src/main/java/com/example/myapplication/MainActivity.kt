a352e52cdfa627edf4378d46896cc138d580ec66
aira0046 <sygdn96@au.com>
2020-12-28 13:56:17 +0900

    箱の形にできた？

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,12 +5,14 @@ import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import io.realm.Realm
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val realm:Realm= Realm.getDefaultInstance()
         originalbutton.setOnClickListener {
             val toRealmIdeaApplicationIntent = Intent(this, RealmIdeaApplication::class.java)
             startActivities(toRealmIdeaApplicationIntent)
