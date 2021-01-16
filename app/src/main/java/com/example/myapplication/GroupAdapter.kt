e9605f7ba18a4b2a0eb1f452b72618e6f92c18dc
aira0046 <sygdn96@au.com>
2021-01-16 17:23:43 +0900

    一時保存

--- app/src/main/java/com/example/myapplication/GroupAdapter.kt
+++ app/src/main/java/com/example/myapplication/GroupAdapter.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Context
 import android.widget.AdapterView
 import io.realm.OrderedRealmCollection
+import io.realm.RealmRecyclerViewAdapter
 
 class GroupAdapter(
     private val cntext: Context,
@@ -10,3 +11,16 @@ class GroupAdapter(
     private val groupList: OrderedRealmCollection<Group>?,
     private val autoUpdate:Boolean
 ):
+        RealmRecyclerViewAdapter<Group,GroupAdapter.GroupViewHolder>(groupList,autoUpdate){
+
+
+    override fun getItemCount(): Int =groupList?.size?:0
+    override fun onBindViewHolder(holder:GroupViewHolder, position: Int) {
+        val group: Group = groupList?.get(position) ?: return
+        holder.titileTextView.text = group.title
+
+        holder.container.setOnClickListener {
+            listener.onItemClick(group)
+        }
+    }
+        }
