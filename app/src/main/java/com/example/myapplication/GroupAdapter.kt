c3b8c335caadbf414071557a2d1c7c7973703675
aira0046 <sygdn96@au.com>
2021-01-30 18:02:10 +0900

    grouplistの開発

--- app/src/main/java/com/example/myapplication/GroupAdapter.kt
+++ app/src/main/java/com/example/myapplication/GroupAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -13,16 +12,15 @@ import io.realm.RealmRecyclerViewAdapter
 import kotlinx.android.synthetic.main.item_group_list.view.*
 
 class GroupAdapter(
-    private val context: Context,
-    private val listener: OnItemClickListener,
-    private val groupList: OrderedRealmCollection<Group>?,
-    private val autoUpdate:Boolean
-):
-        RealmRecyclerViewAdapter<Group,GroupAdapter.GroupViewHolder>(groupList,autoUpdate){
+        private val context: Context,
+        private val listener: OnItemClickListener,
+        private val groupList: OrderedRealmCollection<Group>?,
+        private val autoUpdate: Boolean
+) :
 
-
-    override fun getItemCount(): Int =groupList?.size?:0
-    override fun onBindViewHolder(holder:GroupViewHolder, position: Int) {
+        RealmRecyclerViewAdapter<Group, GroupAdapter.GroupViewHolder>(groupList, autoUpdate) {
+    override fun getItemCount(): Int = groupList?.size ?: 0
+    override fun onBindViewHolder(holder: GroupViewHolder, position: Int) {
         val group: Group = groupList?.get(position) ?: return
         holder.titleTextView.text = group.title
 
@@ -32,18 +30,18 @@ class GroupAdapter(
     }
 
     override fun onCreateViewHolder(
-        viewGroup: ViewGroup,
-        viewType: Int): GroupAdapter.GroupViewHolder {
-        val v = LayoutInflater.from(context).inflate(R.layout.item_group_list,viewGroup,false)
+            viewGroup: ViewGroup,
+            viewType: Int): GroupAdapter.GroupViewHolder {
+        val v = LayoutInflater.from(context).inflate(R.layout.item_group_list, viewGroup, false)
         return GroupViewHolder(v)
     }
 
-    class GroupViewHolder(view: View):RecyclerView.ViewHolder(view){
-    val titleTextView: TextView = view.titleTextView
-     val container:LinearLayout= view.container
+    class GroupViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView = view.titleTextView
+        val container: LinearLayout = view.container
     }
 
-    interface OnItemClickListener{
-        fun onItemClick(item:Group)
+    interface OnItemClickListener {
+        fun onItemClick(item: Group)
     }
 }
--- app/src/main/java/com/example/myapplication/GroupListActivity.kt
+++ app/src/main/java/com/example/myapplication/GroupListActivity.kt
@@ -2,10 +2,63 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import io.realm.Realm
+import io.realm.RealmRecyclerViewAdapter
+import io.realm.RealmResults
+import java.util.*
+import kotlinx.android.synthetic.main.activity_group_list.*
 
 class GroupListActivity : AppCompatActivity() {
+
+    private val realm: Realm by lazy {
+        Realm.getDefaultInstance()
+
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_list)
+
+        val groupList: RealmResults<Group> = readAll()
+        val listener = object :
+            GroupAdapter.OnItemClickListener {
+            override fun onItemClick(item: Group) {
+                TODO("Not yet implemented")
+            }
+        }
+        val adapter = GroupAdapter(
+            this,
+            listener,
+            groupList,
+            true
+        )
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = adapter
+
     }
 }
+
+
+    override fun OnDestroy(){
+    super.onDestroy()
+    realm.close()
+}
+
+        fun readAll():RealmResults<Group>{
+            return realm.where(Group::class.java).findAll().short("createdAt",sort.ASCENDING)
+        }
+fun create(title:String){
+    realm.executeTransaction{
+        val group:Group = it.createObject(
+            Group::class.java, UUID.randomUUID()tostring()
+        )
+        group.title = title
+    }
+}
+
+
